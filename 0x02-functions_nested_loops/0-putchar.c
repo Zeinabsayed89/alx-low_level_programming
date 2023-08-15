d59b98648b1121ee50dddd669e667b7fee0c3a4c
“zeinabsayed” <zeinabsayed1@yahoo.com>
2023-08-15 14:15:48 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+/ Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("_putchar");
+
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_F
+#define MAIN_F
+#include <stdio.h>
+
+int _putchar (char c);
