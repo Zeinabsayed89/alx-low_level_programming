0a79a890b3989d5a179f76b8db2368ced974b0fc
“zeinabsayed” <zeinabsayed1@yahoo.com>
2023-08-17 16:18:14 -0700

    1010

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
@@ -28,23 +28,4 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-}
-
-
-10-main.c
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
 }
